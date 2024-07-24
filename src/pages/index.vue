2a7bb15c5e811f44bdc7f8707e5c77c1dc7f6685
Lemo <lemoggy@foxmail.com>
2024-07-24 23:07:30 +0800

    add

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,63 +1,118 @@
+<!-- eslint-disable vue/component-name-in-template-casing -->
+<!-- eslint-disable no-console -->
 <script setup lang="ts">
-import { headerRequest } from '~/api/common'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import SwiperCore, { Autoplay, EffectCoverflow, Navigation } from 'swiper/core'
+import 'swiper/css'
+import { companyDetail, headerRequest } from '~/api/common'
 
 defineOptions({
   name: 'IndexPage',
 })
+
+SwiperCore.use([EffectCoverflow, Navigation, Autoplay])
+
+const pageData = ref<any>(null)
+const loading = ref(true)
+const error = ref<string | null>(null)
 headerRequest({ type: 1 })
+
+onMounted(async () => {
+  try {
+    const result: any = await companyDetail({ type: 1 })
+    pageData.value = result.data
+  }
+  catch (err: any) {
+    error.value = err.message
+  }
+  finally {
+    loading.value = false
+  }
+})
+
+const onSwiper: any = (swiper: any) => {
+  console.log(swiper)
+}
+const onSlideChange: any = () => {
+  console.log('slide change')
+}
+
+const coverflowEffect = {
+  rotate: 0, // slide做3d旋转时Y轴的旋转角度。默认50。
+  stretch: 50, // 每个slide之间的拉伸值，越大slide靠得越紧。
+  depth: 100, // slide的位置深度。值越大z轴距离越远，看起来越小。
+  modifier: 2, // depth和rotate和stretch的倍率，相当于depth*modifier、rotate*modifier、stretch*modifier，值越大这三个参数的效果越明显。默认1。
+  slideShadows: false, // 开启slide阴影。默认 true。
+}
 </script>
 
 <template>
   <div class="company-page">
-    <img class="absolute relative z-1 mx-auto" src="/company.png" alt="">
-    <div class="absolute top-25 z-2 w-full">
-      <section class="h-125 bg-op-30" style="background-image: url(/)">
-        <TheAlignContainer class="h-full bg-blue bg-op-30 pt-45">
+    <!-- <img class="absolute relative z-1 mx-auto" src="/company.png" alt=""> -->
+    <div class="w-full">
+      <section class="h-125 bg-center bg-no-repeat" :style="`background-image: url(${pageData?.pc_cover})`">
+        <TheAlignContainer class="h-full pt-45">
           <div class="mb-0 text-[5.5rem] text-white font-700">
-            公司介绍
+            {{ pageData?.pc_writing }}
           </div>
         </TheAlignContainer>
       </section>
       <div class="company-info">
         <TheAlignContainer>
           <div class="pb-10 pt-12 text-center text-[2.2rem]">
-            企 ｜ 业 ｜ 介 ｜ 绍
+            {{ pageData?.content_title }}
           </div>
-          <div class="description indent-8 text-xl text-gray-600 font-thin leading-8">
-            <p>
-              傲强集团旗下子公司，自2018年起其母公司总部位于中国永年。河北傲强金属制品集团有限公司下设邯郸市嘉冠金属制品有限公司、邯郸胜冠金属制品制造有限公司。南京傲强机电安装工程有限公司、北京钇嘉国际贸易有限公司。公司占地面积10000平方米，现有员工500余人。
-            </p>
-            <p>
-              北京钇嘉国际贸易有限公司是一家专业提供一站式智慧能源解决方案、全套家庭储能配置、太阳能电器、商用储能的企业。工业光伏储能研发、生产、销售、安装一站式服务。
-              高质量的产品和出色的客户服务帮助该组织获得了覆盖亚洲、南美、欧洲和非洲的全球销售网络，并且正在快速增长。 如果您对我们的任何产品感兴趣或想讨论定制订单，请随时与我们联系。
-              我们期待与世界各地的新客户建立成功的业务关系。
-            </p>
-          </div>
+          <div class="description indent-8 text-xl text-gray-600 font-thin leading-8" v-html="pageData?.content" />
           <div class="pb-20 pt-6">
-            <img class="mx-auto h-100 w-full" src="https://fakeimg.pl/1200x400/282828/?text=%E5%8D%A0%E4%BD%8D%E5%9B%BE&font=noto" alt="" srcset="">
+            <img class="mx-auto" :src="pageData?.content_img" alt="" srcset="">
           </div>
         </TheAlignContainer>
       </div>
-      <div class="company-photo-wall h-180 bg-blue bg-op-30">
+      <div class="company-photo-wall h-180 bg-[#E7F0FF]">
         <TheAlignContainer>
           <div class="pb-10 pt-14 text-center text-[2.2rem]">
-            公 ｜ 司 ｜ 概 ｜ 况
+            {{ pageData?.company_overview_title }}公 ｜ 司 ｜ 概 ｜ 况
           </div>
-          <div class="photo-swiper">
-            aaa
+          <div class="pb-10 text-center text-xl text-neutral-500 font-thin">
+            {{ pageData?.company_overview_content }}公司概况内容公司概况内容公司概况内容
+          </div>
+          <div class="photo-swiper relative">
+            <Swiper
+              :slides-per-view="2" :centered-slides="true" effect="coverflow"
+              :coverflow-effect="coverflowEffect" :loop="true" :navigation="{ nextEl: '.bus-next', prevEl: '.bus-prev' }"
+              @swiper="onSwiper" @slide-change="onSlideChange"
+            >
+              <SwiperSlide v-for="i in 5" :key="i">
+                <img src="https://fakeimg.pl/600x400/282828/eae0d0/?text=%E4%BA%A7%E5%93%81%E5%9B%BE&font=noto" alt="" srcset="">
+                <div class="pt-7 text-center text-xl text-neutral-500">
+                  {{ `slide ${i}` }}
+                </div>
+              </SwiperSlide>
+            </Swiper>
+            <div class="btn-prev bus-prev btn" />
+            <div class="btn-next bus-next btn" />
           </div>
         </TheAlignContainer>
       </div>
       <div class="company-aptitude h-180">
         <TheAlignContainer>
           <div class="pb-4 pt-8 text-center text-[2.2rem]">
-            资 ｜ 质 ｜ 认 ｜ 证
+            {{ pageData?.company_qualifications_title }}资 ｜ 质 ｜ 认 ｜ 证
           </div>
-          <div class="text-center text-xl text-neutral-500 font-thin">
-            文案需要修改文案需要修改文案需要修改文案需要修改文案需要修改文案需要修改文案
+          <div class="pb-10 text-center text-xl text-neutral-500 font-thin">
+            {{ pageData?.company_qualifications_content }}文案需要修改文案需要修改文案需要修改文案需要修改文案需要修改文案需要修改文案
           </div>
-          <div class="aptitude-swiper">
-            bbb
+          <div class="aptitude-swiper relative">
+            <Swiper
+              :slides-per-view="4" :space-between="30" :loop="true"
+              :navigation="{ nextEl: '.bus-next2', prevEl: '.bus-prev2' }"
+            >
+              <SwiperSlide v-for="i in 5" :key="i">
+                <img src="https://fakeimg.pl/280x400/282828/eae0d0/?text=%E4%BA%A7%E5%93%81%E5%9B%BE&font=noto" alt="" srcset="">
+              </SwiperSlide>
+            </Swiper>
+            <div class="btn-prev bus-prev2 btn" />
+            <div class="btn-next bus-next2 btn" />
           </div>
         </TheAlignContainer>
       </div>
@@ -72,5 +127,38 @@ headerRequest({ type: 1 })
 
 .description > p {
   @apply mb-8;
+}
+
+.photo-swiper .swiper-slide {
+  height: 500px;
+}
+
+.aptitude-swiper .swiper-slide {
+  width: 280px;
+  height: 400px;
+}
+
+.btn {
+  width: 57px;
+  height: 57px;
+  position: absolute;
+}
+
+.btn-prev {
+  @apply absolute top-2/4 -mt-10 -left-20;
+  background: url(../assets/zuo-1.png) no-repeat center;
+
+  &:hover {
+    background: url(../assets/zuo-2.png) no-repeat center;
+  }
+}
+
+.btn-next {
+  @apply absolute top-2/4 -mt-10 -right-20;
+  background: url(../assets/you-1.png) no-repeat center;
+
+  &:hover {
+    background: url(../assets/you-2.png) no-repeat center;
+  }
 }
 </style>
